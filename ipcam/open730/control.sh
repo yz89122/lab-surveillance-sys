b4b4f8a16b9ec9fec39d280249bc02c0f72062e7
Yz <yanzhen610@gmail.com>
2018-10-20 22:43:36 +0800

    fixs problem when not functional key pressed

--- ipcam/open730/control.sh
+++ ipcam/open730/control.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+ESC=$(echo -ne '\x1b')
+
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] ;
 then
   echo "Usage: $0 <hostname> <http-port> <username> [password]"
@@ -25,14 +27,19 @@ key_to_code() {
   echo -n $1 | grep -E '\[B' && return 2
   echo -n $1 | grep -E '\[C' && return 4
   echo -n $1 | grep -E '\[D' && return 6
+  echo -n $1 | grep -E '\[F' && return 1
   return 1
 }
 
+echo Press arrow key to control the rotating direction
+echo Press \[End\] key to send stop signal
+
 while :
 do
-  read -sn3 key
-  key_to_code $key
+  until read -sN1 k1 && [ "$k1" = "$ESC" ]; do false; done
+  read -sN2 k2
+  key_to_code $k1$k2
   command=$?
-  echo -n command $command' '
+  echo -n sending command $command' '
   send_command $(echo -n $command)
 done
