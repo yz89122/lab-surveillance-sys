12f6a1e28547414cb0938dc3ff73d00bb914a574
Yz. Chen <yanzhen610@gmail.com>
2019-03-17 00:59:28 +0800

    refactored `fetcher.py`

--- ipcam/ip540/fetcher.py
+++ ipcam/ip540/fetcher.py
@@ -4,12 +4,12 @@ class StreamingFetcher:
     def __init__(self, streaming_address):
         self.url = 'rtsp://' + streaming_address + '/medias1'
         self.frame = None
-        self.__video_capture = cv2.VideoCapture(self.url)
 
     def get_newest_frame(self):
         return self.frame
     
     def start_capturing(self):
+        self.__video_capture = cv2.VideoCapture(self.url)
         if not self.__video_capture.isOpened():
             self.__video_capture.open(self.url)
         while self.__video_capture.isOpened():
